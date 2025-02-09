4d491fdeb8232c2243515ee4021e98d96b13d663
Lix Zhou <xeontz@gmail.com>
2025-02-09 21:22:07 +0800

    Deprecated calling to async_forward_entry_setup
    await async_forward_entry_setups instead

--- custom_components/ha_win_wol/__init__.py
+++ custom_components/ha_win_wol/__init__.py
@@ -46,10 +46,7 @@ async def async_setup_entry(hass, entry):
 
     _LOGGER.info(f"创建设备完成IP: {ip}, Name:{name}, Mac:{mac}, Status: {status}")
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
